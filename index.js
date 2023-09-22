c0671cb6984409042868a3d6000e2ae03d89899a
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-09-22 18:49:26 +0300

    Practice

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Follow along with the examples here
+//function sayHello(){
+    //console.log("Hello!")
+//}
+
+//function sayHelloToIsabel(){
+    //console.log("Hello Isabel!")
+//}
+
+//function sayHelloToSofia(){
+    //console.log("Hello Sofia!")
+//}
+
+//function sayHelloToBrendan(){
+    //console.log("Hello Brendan!")
+//}
+
+//sayHelloToIsabel()
+//sayHelloToSofia()
+//sayHelloToBrendan()
+
+//function sayHelloTo(firstName){
+    //console.log(`Hello, ${firstName}!`)
+//}
+
+//console.log(firstName)
+//sayHelloTo("Isabel")
+//sayHelloTo(1)
+
+//function say(greeting, firstName){
+    //console.log("FirstName: ", firstName)
+    //console.log("Greeting: ", greeting)
+    //console.log(`${greeting}, ${firstName}`)
+//}
+//say("Julio", "Goodbye")
+
+//function add(x, y){
+   //return x + y
+//}
+
+//console.log(add(1, 2))
+
+//function say(greeting, firstName){
+    //console.log(`${greeting}, ${firstName}!`)
+//}
+
+//console.log(say("Hello", "Sofia"))
+
+function say(greeting, firstName) { 
+    //console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+  }
+say("Howdy", "partner");
